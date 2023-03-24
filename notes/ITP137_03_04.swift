eb10817c27d3321d3533e7bda5ffef4f07f5d68d
TTaylor96 <124452401+TTaylor96@users.noreply.github.com>
2023-03-24 23:41:14 +0000

    Optimized "condition" assignment in 03_04 notes

--- notes/ITP137_03_04.swift
+++ notes/ITP137_03_04.swift
@@ -5,21 +5,32 @@ print("\nEnter a string of characters:")
 var test_string = readLine()!
 var highest_count = 0
 var highest_char: Character = "_"
+var found_chars: String = ""
 
-// test_string.forEach { char in
 for char in test_string{
-  var test_index = char
   var test_count = 0
-  // test_string.forEach { test_char in
-  for test_char in test_string{
-    if test_index == test_char{
-      test_count += 1
+  var new_char = true
+  // checks to see if character has already been counted
+  for old_char in found_chars{
+    if char == old_char{
+      new_char = false
+      break
+    }
+  }
+  // if character is new, append to string and count how many
+  if new_char{
+    found_chars.append(char)
+    for test_char in test_string{
+      if char == test_char{
+        test_count += 1
+      }
     }
   }
   if test_count > highest_count{
      highest_count = test_count
      highest_char = char
   }
+  test_string.removeFirst() //characters that have been tested are removed from the initial string
 }
 
 print("\nMost common character: " + String(highest_char) + "\nCount of highest character: " + String(highest_count))
